748e428e1b19e71c6b15a545cffd4e159b0d0b28
vickyrockstar <116479689+vickyrockstar@users.noreply.github.com>
2023-03-23 12:14:53 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import "../styles/App.css";
 import Child from "./Child";
 const App = () => {
-    function incrementAge() {}
+    const [age, setAge] = useState(19);
+    function incrementAge() {
+        setAge(age + 1);
+    }
 
-    return <Child />;
+    return <Child age={age} incrementAge={incrementAge}/>;
 };
 
 export default App;
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Child() {
-    return <div id="child"></div>;
+const {age, incrementAge} = props;
+function handleClick(){
+    incrementAge();
+}
+    return <div id="child">
+        <p id="text">Today I am {age} Years of Age</p>
+        <button id="button" onClick={handleClick} >Increment Age</button>
+    </div>;
 }
 
 export default Child;
